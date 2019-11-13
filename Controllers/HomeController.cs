abcfe346b4b0ac405094ac28eedb5b4d81eaf0ab
Saparbek Bainazar <Saparbek28@gmail.com>
2019-11-13 14:22:52 +0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }  
+        
+        // GET: Home/Columns
+        public ActionResult Columns()
+        {
+            return View();
+        }
+        // GET: Home/Left
+
+        public ActionResult LeftSideBar()
+        {
+            return View();
+        }  
+        public ActionResult rightSidebar()
+        {
+            return View();
+        }
+        
+        public ActionResult withoutSidebar()
+        {
+            return View();
+        }
+
+
+        
+
+    }
+}
